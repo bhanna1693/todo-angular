9ff4031b16ec12cf48d96ec0b0091a05f0057da5
brianhanna <bhanna1693@gmail.com>
2023-08-23 17:08:39 -0400

    moved todo to feature folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {TodoComponent} from "./todo/todo.component";
+import {TodoComponent} from "./features/todo/todo.component";
 
 const routes: Routes = [
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {TodoComponent} from './todo/todo.component';
-import {TodoQuickAddComponent} from './todo/todo-quick-add/todo-quick-add.component';
+import {TodoComponent} from './features/todo/todo.component';
+import {TodoQuickAddComponent} from './features/todo/todo-quick-add/todo-quick-add.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 
